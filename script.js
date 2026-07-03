3ca9998cc7d2d4235a4cc00bbe96a1f54531d56c
naimulislam123 <namul660@gmail.com>
2026-07-03 20:04:18 +0600

    Add birthday image to birthday message

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ document.querySelector(".gift").addEventListener("click", () => {
   document.body.innerHTML = `
   <div style="height:100vh;background:linear-gradient(135deg,#ff4fa3,#ffb6d9,#ffd6eb);display:flex;flex-direction:column;justify-content:center;align-items:center;text-align:center;color:white;padding:20px;">
     <h1 style="font-size:45px;">🎉 Happy Birthday Jesmin 🎂</h1>
-
+<img src="photo.jpg" class="photo">
     <h2 style="margin-top:15px;">You are truly special! 💖</h2>
 
     <p style="margin-top:25px;font-size:20px;max-width:700px;line-height:1.8;">
